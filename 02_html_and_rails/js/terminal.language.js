1e61916626a216248e03b0c36888352bcc20b64f
Paul Barry <mail@paulbarry.com>
2013-09-11 11:46:00 +0200

    Completed slides for HTML lesson in Unit 2.  Re-organized order of units.

---
+++ 02_html_and_rails/js/terminal.language.js
@@ -0,0 +1,22 @@
+/*
+  Language: terminal console
+  Author: Josh Bode <joshbode@gmail.com>
+*/
+
+hljs.LANGUAGES.terminal = {
+  contains: [
+    {
+      className: 'string',
+      begin: '^([\\w.]+)@([\\w.]+)'
+    },
+    {
+      className: 'constant',
+      begin: ' (.*) \\$ '
+    },
+    {
+      className: 'ansi',
+      begin: '<span style\\="([^"]+)">',
+      end: '<\\/span>'
+    }
+  ]
+};
